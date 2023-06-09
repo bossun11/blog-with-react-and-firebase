e5b1b06c67225f3112eb4b971d61ac7aeaa69b3c
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-09 11:32:17 +0900

    Add: Googleでのソーシャルログインを実装

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+
+  const loginWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
