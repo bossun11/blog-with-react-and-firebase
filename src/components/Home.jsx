516ad92bd7ab18ed72ceaaaa6b1c49cdb4905932
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-09 16:29:17 +0900

    Fix: Firebaseから取得したデータをホームコンポーネントに出力するように変更

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import "./Home.css";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">今はReactの学習中です</div>
-        <div className="nameAndDeleteButton">
-          <h3 className="postedUserName">つっつん</h3>
-          <button className="deleteButton">削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3 className="postedUserName">{post.author.username}</h3>
+              <button className="deleteButton">削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
